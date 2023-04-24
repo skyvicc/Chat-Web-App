d457d494d461fdca13d0242273f40598ab4fb376
leTabs <125308756+leTabs@users.noreply.github.com>
2023-04-24 21:57:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, session 
 import sqlite3
 
 app = Flask(__name__, static_folder='static')
