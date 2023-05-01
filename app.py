f48eea0494979a05d1a039d18468a58f6e6f86a3
leTabs <125308756+leTabs@users.noreply.github.com>
2023-05-01 21:16:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,97 +1,101 @@
-from flask import Flask, render_template, request, session, redirect, url_for
-from flask_socketio import join_room, leave_room, send, SocketIO
-import random
-from string import ascii_uppercase
+from flask import Flask, render_template, redirect, url_for, request, session
+import sqlite3, hashlib, os
+from datetime import timedelta
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "bobby"
-socketio = SocketIO(app)
-rooms = {}
+app = Flask(__name__, static_folder='static')
+app.secret_key = os.urandom(16)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=120)
 
-def generate_unique_code(Length):
-    while True:
-        code = ""
-        for _ in range(Length):
-            code += random.choice(ascii_uppercase)
-        if code not in rooms:
-            break
-    return code
-
-@app.route('/', methods=["POST", "GET"])
-def home():
-    session.clear()
-    if request.method =="POST":
-        name= request.form.get("name")
-        code = request.form.get("code")
-        join = request.form.get("join", False)
-        create = request.form.get("create", False)
-        if not name:
-            return render_template("home.html", error="Please enter a name...", code=code, name=name)
-            #----------------------------------------------------------------
-        if join !=False and not code:
-            return render_template("home.html", error = "Please enter  a room code", code=code, name=name)
-        room = code
-
-        if create != False:
-            room = generate_unique_code(4)
-            rooms[room] = {"members": 0,  "messages":[]}
-        elif code not in rooms:
-            return render_template('home.html', error="Room does  not exist", code=code, name=name)
+conn = sqlite3.connect('profiles.db')
+cursor = conn.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS users (
+        username TEXT UNIQUE NOT NULL,
+        password TEXT NOT NULL
+    )
+""")
+conn.commit()
+conn.close()
 
 
-        session["room"] = room
-        session["name"] = name
-        return redirect(url_for("room"))
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    return render_template("home.html")
+@app.route('/login')
+def login():
+    return render_template('logIn.html')
+@app.route('/signup')
+def signup():
+    return render_template('signUp.html')
+#========================================================================
+@app.route('/login_step', methods=['POST'])
+def process_login():
+    # get the login form data
+    username = request.form['username']
+    password = request.form['pass']
+    stay_logged = request.form.get('stayLogged')
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT password FROM users WHERE username = ?", (username,))
+    result = cursor.fetchone()
 
-@app.route("/room")
-def room():
-    room = session.get("room")
-    if room is None or session.get("name") is None or room not in rooms:
-        return redirect(url_for("home"))
-    return render_template("room.html", code=room, messages = rooms[room]["messages"])
+    if result is None:
+        conn.close()
+        error = 'Invalid username or password'
+        return render_template('errorNotification.html', error = error)
+    else:
+        hashed_password = result[0]
+        hashed_input_password = hashlib.sha256(password.encode()).hexdigest()
 
+        if hashed_input_password != hashed_password:
+            conn.close()
+            error = 'Invalid username or password'
+            return render_template('errorNotification.html', error = error)
+        else:
+            session['username'] = username
+            conn.close()
 
-@socketio.on("message")
-def message(data):
-    room = session.get("room")
-    if room not in rooms:
-        return
-    content = {"name" : session.get("name"),
-    "message" : data["data"]}
-    send(content, to=room)
-    rooms[room]["messages"].append(content)
-    print(f'{session.get("name")} said : {data["data"]}')
+            session['logged_in'] = True
+            session.modified =True
+            return redirect(url_for('success'))
+#------------------------------------------------------------------
+@app.route('/register', methods=['POST'])
+def process_signup():
+    # code to process signup form data here
+    username = request.form['username']
+    password = request.form['password']
+    password_confirm = request.form['password_confirm']
+    stay_logged = request.form.get('stayLogged')
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    if cursor.fetchone() is not None:
+        conn.close()
+        error = 'Username already exists'
+        return render_template('errorNotification.html', error = error)
+    elif password != password_confirm:
+        error = 'The passwords don\'t match'
+        return render_template('errorNotification.html', error = error)
 
-@socketio.on("connect")
-def connect(auth):
-    room = session.get("room")
-    name = session.get("name")
-    if not room or not name:
-        return
-    if room not in rooms:
-        leave_room(room)
-        return
-    join_room(room)
-    send({"name": name, "message": "has enetered the room"}, to=room)
-    rooms[room]["members"] += 1
-    print(f'{name} joined the room  {room}')
+    else:
+        hashed_password = hashlib.sha256(password.encode()).hexdigest()
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+        conn.commit()
+        session['username'] = username
+        conn.close()
+        return redirect(url_for('success'))
+@app.route(f'/find')
+def success():
+    if 'logged_in' not in session:
+        return redirect(url_for('login'))
+    conn = sqlite3.connect('profiles.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT username FROM users')
+    usernames = [row[0] for row in cursor.fetchall()]
+    conn.close()
+    user = session.get('username')
+    return render_template('findSomeone.html', usernames=usernames, user=user)
 
-@socketio.on("disconnect")
-def disconnect():
-    room = session.get("room")
-    name = session.get("name")
-    leave_room(room)
-
-    if room in rooms:
-        rooms[room]["members"] -= 1
-        if rooms[room]["members"] <= 0:
-            del rooms[room]
-    send({"name": name, "message": "has left the room"}, to=room)
-    print(f'{name} left the room {room}')
-
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True,allow_unsafe_werkzeug=True)
-    app.close()
+if __name__ == '__main__':
+    app.run()
